d11fadd95118f71430847e0e76328f140b31acf5
Eli Schiff <eli@elischiff.org>
2020-03-07 15:47:06 -0500

    Now compiles to a tmp folder

--- run_memory_dump.py
+++ run_memory_dump.py
@@ -3,6 +3,7 @@ import os
 
 if __name__ == "__main__":
     arguments = " ".join(sys.argv[1::])
-    os.system("g++ " + arguments + " -I/work/drmemory/releases/DrMemory-Linux-2.3.18322/drmf/include /work/drmemory/releases/DrMemory-Linux-2.3.18322/drmf/lib64/release/libdrmemory_annotations.a")
-    os.system("/work/drmemory/releases/DrMemory-Linux-2.3.18322/bin64/drmemory -brief -- ./a.out")
-    os.system("rm ./a.out")
+    os.system("mkdir -p /tmp/memory_diagram")
+    os.system("rm -rf /tmp/memory_diagram/*")
+    os.system("g++ " + arguments + " -I/work/drmemory/releases/DrMemory-Linux-2.3.18322/drmf/include /work/drmemory/releases/DrMemory-Linux-2.3.18322/drmf/lib64/release/libdrmemory_annotations.a -o /tmp/memory_diagram/a.out")
+    os.system("/work/drmemory/releases/DrMemory-Linux-2.3.18322/bin64/drmemory -brief -- /tmp/memory_diagram/a.out")
