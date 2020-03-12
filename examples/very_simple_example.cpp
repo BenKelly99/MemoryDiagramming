5e624a17ddd5ec6ba2979563aeaaa98de2f70013
Ben Kelly <benjaminkelly@me.com>
2020-03-11 20:36:02 -0400

    added docs for dwarf so i dont have to do stuff

--- examples/very_simple_example.cpp
+++ examples/very_simple_example.cpp
@@ -1,15 +1,15 @@
 #include "drmemory_annotations.h"
 
 int main(){
-    long x = 1;
-    long* y = new long;
-    *y = 2;
-    long* z = new long[3];
-    z[0] = 3;
-    z[1] = 4;
-    z[2] = 5;
-    long** w = new long*;
-    *w = y;
-    long* v = z + 1;
+    long var_x = 1;
+    long* var_y = new long;
+    *var_y = 2;
+    long* var_z = new long[3];
+    var_z[0] = 3;
+    var_z[1] = 4;
+    var_z[2] = 5;
+    long** var_w = new long*;
+    *var_w = var_y;
+    long* var_v = var_z + 1;
     DRMEMORY_ANNOTATE_DUMP_MEMORY_LAYOUT();
 }
--- run_memory_dump.py
+++ run_memory_dump.py
@@ -209,7 +209,7 @@ if __name__ == "__main__":
     arguments = " ".join(sys.argv[1::])
     os.system("mkdir -p /tmp/memory_diagram")
     os.system("rm -rf /tmp/memory_diagram/*")
-    os.system("g++ " + arguments + " -O0 -I/work/drmemory/releases/DrMemory-Linux-2.3.18322/drmf/include /work/drmemory/releases/DrMemory-Linux-2.3.18322/drmf/lib64/release/libdrmemory_annotations.a -o /tmp/memory_diagram/a.out")
+    os.system("g++ " + arguments + " -g -O0 -I/work/drmemory/releases/DrMemory-Linux-2.3.18322/drmf/include /work/drmemory/releases/DrMemory-Linux-2.3.18322/drmf/lib64/release/libdrmemory_annotations.a -o /tmp/memory_diagram/a.out")
     result = subprocess.run("/work/drmemory/releases/DrMemory-Linux-2.3.18322/bin64/drmemory -brief -- /tmp/memory_diagram/a.out", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     json_files = re.findall(r'^\s*~~Dr\.M~~ Memory layout written to: (\/tmp\/Dr\. Memory\/DrMemory-a\.out\..*?\.json)$', result.stderr.decode('utf-8'), re.MULTILINE)
     json_outputs = []
